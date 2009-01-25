bfe4f1393907affffd5e3515a0aef0a5799a9bd3
Simon Urbanek <Simon.Urbanek@r-project.org>
2009-01-25 00:00:00 +0000

    version 0.6-5

--- src/Rsrv.h
+++ src/Rsrv.h
@@ -1,6 +1,6 @@
 /*
  *  Rsrv.h : constants and macros for Rserve client/server architecture
- *  Copyright (C) 2002-10 Simon Urbanek
+ *  Copyright (C) 2002-11 Simon Urbanek
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published
@@ -18,7 +18,7 @@
  *  Note: This header file is licensed under LGPL to allow other
  *        programs to use it under LGPL. Rserve itself is licensed under GPL.
  *
- *  $Id: Rsrv.h 310 2011-05-02 13:40:15Z urbanek $
+ *  $Id: Rsrv.h 317 2011-06-08 18:37:46Z urbanek $
  */
 
 /* external defines:
@@ -32,7 +32,7 @@
 #include "config.h"
 #endif
 
-#define RSRV_VER 0x000604 /* Rserve v0.6-4 */
+#define RSRV_VER 0x000605 /* Rserve v0.6-5 */
 
 #define default_Rsrv_port 6311
 
